57f724d620b5f0d881f3632f54c440ccd7638ea4
praveen singh <rawatpraveen477@gmail.com>
2024-02-11 10:04:56 +0530

    responsiv menubaar

--- script.js
+++ script.js
@@ -29,16 +29,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // for responsive navbaaar
 
-document.getElementById('mobile-menu').addEventListener('click', function () {
-   let navbar = document.querySelector('.nav_item');
-   let li = document.querySelectorAll('.the ul li')
-   if (navbar.style.display === 'block') {
-      navbar.style.display = 'none';
+// document.getElementById('mobile-menu').addEventListener('click', function () {
+//    let navbar = document.querySelector('.nav_item');
+//    let li = document.querySelectorAll('.the ul li')
+//    if (navbar.style.display === 'block') {
+//       navbar.style.display = 'none';
 
-   } else {
+//    } else {
+//       navbar.style.display = 'block';
+//    }
+// });
+
+ let menubar = document.getElementById('mobile-menu')
+ menubar.addEventListener("click",()=>{
+   let navbar = document.querySelector('.nav_item');
+   if (navbar.style.display === 'none'){
       navbar.style.display = 'block';
    }
-});
+   else{
+      navbar.style.display = 'none';
+   }
+})
+
+
+
+
+
 
 
 // skills loader
